c46f9fd95a94d1f4b5f435b9753ee8b9aed343f6
Tomasz Łasica <tlasica@o2.pl>
2012-12-19 14:06:22 +0100

    Initial version, with simple layout and buttons

---
+++ src/pl/tlasica/moodblog/MainActivity.java
@@ -0,0 +1,24 @@
+package pl.tlasica.moodblog;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
